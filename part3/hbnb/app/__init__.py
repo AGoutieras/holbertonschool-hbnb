f125e3f414ffe5ef92c6fa9f859dbda14fd156c5
AGoutieras <goutieras.anthony@gmail.com>
2026-03-07 22:18:40 +0100

    feat: initialize JWTManager and register auth namespace

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.auth import api as auth_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     # Register the users namespace
@@ -23,7 +27,10 @@ def create_app(config_class="config.DevelopmentConfig"):
     # Register the reviews namespace
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
-    # Register the amenites namespace
+    # Register the amenities namespace
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    
+    # Register the auth namespace
+    api.add_namespace(auth_ns, path='/api/v1/auth')
 
     return app
